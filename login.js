900156438716204b953653be16aa501cc6a8cde6
Guilherme Garcia de Moraes <guilhermegarciademoraes@gmail.com>
2023-09-08 14:40:55 -0300

    criação de login.js e alteracao de login.html

---
+++ login.js
@@ -0,0 +1,36 @@
+
+        function validaDados() {
+            let vnome = document.getElementById('nome')
+            if (vnome.value == '') {
+                alert('Informe nome')
+                vnome.focus()
+                return
+            }
+
+            let vemail = document.getElementById('email')
+            if (vemail.value == '') {
+                alert('informe email')
+                vemail.focus()
+                return
+            }
+
+            let ec = document.getElementsByName('ec')
+            /*alert(ec[0].checked)
+            alert(ec[1].checked)*/
+            if (ec[0].checked != true && ec[1].checked != true) {
+                alert('informe seu pacote')
+                return
+            }
+
+            let pref = document.getElementsByName('pref')
+            //alert(pref.length)
+            let opcoes = ''
+            for (let i = 0; i < pref.length; i++) {
+                if (pref[i].checked) {
+                    opcoes = opcoes + ' - ' + pref[i].value
+                }
+                
+            }
+            
+            alert('Dados salvo com sucesso')
+        }
